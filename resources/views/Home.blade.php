4267455ec03c0c3f317b6147fb005287b3603cd0
Alamgir <alamrs16@gmail.com>
2022-05-11 13:45:33 +0600

    laravel file upload first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,18 @@
+@extends('Layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6 mt-5">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h6>Laravel Ajax File Upload</h6>
+                    </div>
+                    <div class="card-body">
+                        <h6>Upload From Heare</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
